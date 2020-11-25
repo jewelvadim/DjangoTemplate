daea185bdc09c8d3b45afa0ae977dd505fb9556e
Вадим Беглов <jewel.vadim@gmail.com>
2020-11-25 17:36:29 +0700

    update

--- project/apps/main/admin/globals.py
+++ project/apps/main/admin/globals.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
-from settings import PROJECTNAME
-
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
-admin.site.site_header = PROJECTNAME
-admin.site.site_title = '{} Admin'.format(PROJECTNAME)
-admin.site.index_title = 'Welcome to {}'.format(PROJECTNAME)
